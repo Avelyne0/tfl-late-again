18643f048f4ee9cb1e8b0e282872a8077c922491
Sam Barrie <sam.dbarrie@gmail.com>
2019-05-27 21:50:12 +0100

    added api request data

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -2,3 +2,16 @@ class Excuse < ActiveRecord::Base
   belongs_to :users
   belongs_to :undergrounds
 end
+
+const app_id = d4a1a955
+const app_key = 752dfb87ef1e3702402c660cecf36992
+
+#all disruptions
+all_disruptions_call = "https://api.tfl.gov.uk/Line/Mode/tube/Disruption?app_key=752dfb87ef1e3702402c660cecf36992&app_id=d4a1a955"
+
+#disruption type
+disruptions = ["Undefined","RealTime","PlannedWork","Information","Event","Crowding","StatusAlert"]
+
+#disruption type api call
+# https://api.tfl.gov.uk/Line/Meta/DisruptionCategories?app_key=752dfb87ef1e3702402c660cecf36992&app_id=d4a1a955
+
--- config/environment.rb
+++ config/environment.rb
@@ -1,17 +1,12 @@
 require 'bundler/setup'
-
 Bundler.require
-
-
-require 'sqlite3'
-require 'pry'
-require_all 'app'
-
 
 ActiveRecord::Base.establish_connection(
   adapter: 'sqlite3',
-  database: "db/development.sqlite"
+  database: 'db/development.sqlite'
 )
 
 ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+require_all 'app'
 
