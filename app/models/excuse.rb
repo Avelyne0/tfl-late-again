43c2d66b6506bec69a84c2ff5ce8b721e0e7e095
Sam Barrie <sam.dbarrie@gmail.com>
2019-05-30 11:10:08 +0100

    added some excuses

--- app/models/excuse.rb
+++ app/models/excuse.rb
@@ -1,5 +1,11 @@
 class Excuse < ActiveRecord::Base
   belongs_to :users
   belongs_to :trips
-  excuses = ["", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", "", ""]
+  @@excuses = [
+    "Sorry I'm late! I had to get off my train as there was a really stinky man",
+    "Hey boss! I'm late? sorry fella, there was a bit of a delay",
+    "I'm really really really sorry, there was about a thousand people trying to get onto the train",
+    "Hey man, I don't know if you saw the news but a travelling band somehow managed to get",
+    "I really don't want to talk about it, there was a fleet of wasps",
+  ]
 end
